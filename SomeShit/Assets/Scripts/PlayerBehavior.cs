8f8a12899717fd47c2dba426f52c2b0546f4565a
Hakem_hmd <hakemhamdoud8@gmail.com>
2022-01-22 15:22:44 +0100

    My onw Version

---
+++ SomeShit/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerBehavior : MonoBehaviour
+{    
+    [Tooltip("The Parent object of all the Tiles")]
+    public GameObject Tiles;
+    [Tooltip("The Button to role the Dice")]
+    public GameObject RollingButton;
+    [Tooltip("The text saying that the Dice is Rolling")]
+    public TextMeshProUGUI WaitingText;
+    [Tooltip("The Number of My Actual Tile")]
+    public TextMeshProUGUI Score;
+
+    private bool DiceRolled = false;
+    private int TileIndex = 0;
+    private int DiceNumber;
+
+    private float Default_Y;
+    private bool Positioned = false;
+
+    void Start()
+    {       
+        transform.position = Tiles.transform.GetChild(0).position + Vector3.up * 0.4f;
+
+        Default_Y = transform.position.y;
+    }
+
+    void Update()
+    {
+        Score.text = $"Score : {TileIndex}";
+
+        Transform Current = Tiles.transform.GetChild(TileIndex);
+
+        //if the dice is rolled i can start moving
+        if (DiceRolled)
+        {            
+            //I start by going up 
+            if(transform.position.y < Default_Y + 1.5f && Positioned == false)
+            {
+                transform.position += Vector3.up * Time.deltaTime;
+            }
+            else
+            {
+
+
+                //Then i move forward till i arrive at The Current Tile position
+
+                if (Mathf.Abs(transform.position.z) < Mathf.Abs(Current.position.z))
+                {
+                    transform.position += Vector3.forward * Time.deltaTime;
+                }
+                else
+                {
+                    //I set positioned to true to start landing on the tile
+
+                    Positioned = true;
+                }
+              
+            }
+
+            if (Positioned)
+            {
+                //Here i will start landing
+
+                if(transform.position.y > Default_Y)
+                {
+                    transform.position += Vector3.down * Time.deltaTime;
+                }
+                else
+                {
+                    Positioned = false;
+                    DiceRolled = false;
+
+                    RollingButton.SetActive(true);
+
+                    WaitingText.enabled = false;
+                }
+            }
+
+        }
+        else
+        {
+            transform.position = Current.position + Vector3.up * 0.4f;
+        }
+    }
+        
+    public void RollTheDice_EventHandler()
+    { 
+        DiceNumber = Random.Range(1, 7);
+
+        TileIndex += DiceNumber;
+
+        DiceRolled = true;
+
+        RollingButton.SetActive(false);
+
+        WaitingText.enabled = true;
+      
+    }
+}
