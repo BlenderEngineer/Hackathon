f508f3637f4ab25492f13b54c70834984e612aff
BlenderEngineer <82451249+BlenderEngineer@users.noreply.github.com>
2022-01-22 14:35:18 +0200

    Fixed intro screen

--- Hackathon/Assets/Scripts/LevelLoader.cs
+++ Hackathon/Assets/Scripts/LevelLoader.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using System.Threading.Tasks;
 
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private GameObject _lobbyCanvas;
     [SerializeField] private Image _progressBar;
+    [SerializeField] private AudioSource Sound;
     private float _target;
 
     public static LevelLoader Instance;
     // Start is called before the first frame update
     void Awake()
     {
+        //Sound = GetComponent<AudioSource>();
+       // Sound.clip = audio;
+        //Sound.Play();
+        StartCoroutine(WaitForSound(Sound));
+
         if (!Instance)
         {
             Instance = this;
@@ -38,14 +45,22 @@ public class LevelLoader : MonoBehaviour
 
         do
         {
-           // await Task.delay(100);
+            await Task.Delay(100);
             _target = scene.progress;
         } while (scene.progress < .9f);
 
-        //await Task.Delay(1000);
+        await Task.Delay(1000);
 
         scene.allowSceneActivation = true;
         _loaderCanvas.SetActive(false);
+    }
+    
+    public IEnumerator WaitForSound(AudioSource Sound)
+    {
+        yield return new WaitUntil(() => Sound.isPlaying == false);
+        // or yield return new WaitWhile(() => audiosource.isPlaying == true);
+        if (gameObject != null)
+            _loaderCanvas.SetActive(false);
     }
 
     private void Update()
